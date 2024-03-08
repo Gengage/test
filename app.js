43614829505d26807fe4add21dbb528d6e4024e6
Gengage <103494625+Gengage@users.noreply.github.com>
2024-03-08 19:40:26 +0900

    3.08일 과제

---
+++ app.js
@@ -0,0 +1,19 @@
+document.querySelector(".navRunway").addEventListener("click", (e) => 
+{
+document.querySelector(".Runway").scrollIntoView({ behavior: "smooth" })
+})
+
+document.querySelector(".navLookbook").addEventListener("click", (e) => 
+{
+    document.querySelector(".lookbook").scrollIntoView({ behavior: "smooth" })
+})
+
+document.querySelector(".navShop").addEventListener("click", (e) => 
+{
+    document.querySelector(".shop").scrollIntoView({ behavior: "smooth" })
+})
+
+document.querySelector(".navLastseason").addEventListener("click", (e) => 
+{
+    document.querySelector(".lastSeason").scrollIntoView({ behavior: "smooth" })
+})
