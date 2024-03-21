a6bd0e28990d4253618ebcced9995b06fa13ae92
Gengage <103494625+Gengage@users.noreply.github.com>
2024-03-21 12:23:21 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -20,17 +20,18 @@ document.querySelector(".navLastseason").addEventListener("click", (e) =>
 
 const modal = document.querySelector('.modal')
 const btnOpenModal=document.querySelector('.wrap')
-const btnCloseModal = document.querySelector('.close-btn')
 
 
 
 btnOpenModal.addEventListener("click", ()=>{
     modal.style.display="flex"
 })
-btnCloseModal.addEventListener("click", ()=>{
-    modal.style.display="none"
+// 모달 바깥 영역 클릭 시 모달 닫기
+window.addEventListener('click', (e) => {
+  if (e.target === modal) {
+      modal.style.display = 'none'
+  }
 })
-
 
 // slide
 const silder = document.querySelector('#slider');
